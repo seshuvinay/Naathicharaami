a31477ccf16fdfdeb08658ea49b702e30a76e282
seshuvinay <seshu.vinay@gmail.com>
2021-10-22 16:56:19 +0530

    cov

--- app/src/main/java/com/pv3/naathicharaami/ContactsAdapter.java
+++ app/src/main/java/com/pv3/naathicharaami/ContactsAdapter.java
@@ -1,6 +1,5 @@
 package com.pv3.naathicharaami;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -9,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
